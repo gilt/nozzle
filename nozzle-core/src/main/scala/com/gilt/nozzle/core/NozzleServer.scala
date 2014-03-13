9603ffd8e8ab465052ee10748cff685d8a699556
Javier Arrieta <javier@techdelivery.es>
2014-03-13 21:17:42 +0000

    Create an actor per connection to be able to add X-Forwarded-For header

--- nozzle-core/src/main/scala/com/gilt/nozzle/core/NozzleServer.scala
+++ nozzle-core/src/main/scala/com/gilt/nozzle/core/NozzleServer.scala
@@ -6,16 +6,13 @@ import akka.util.Timeout
 import scala.concurrent.duration._
 import spray.can.Http
 import akka.io.IO
-import spray.http.{HttpHeader, HttpResponse, HttpRequest}
+import spray.http.{HttpResponse, HttpRequest}
 import spray.client.pipelining.sendReceive
 import com.gilt.nozzle.core.DevInfo.DevInfoExtractor
 import com.gilt.nozzle.core.TargetInfo.TargetInfoExtractor
 import scala.concurrent.ExecutionContext.Implicits.global
 import PolicyValidator._
 import akka.event.LoggingAdapter
-import scala.concurrent.Future
-import java.net.InetAddress
-import spray.http.HttpHeaders.RawHeader
 
 trait NozzleServer extends App {
 
@@ -40,7 +37,7 @@ trait NozzleServer extends App {
 
   def forwardRequest: ForwardRequest = sendReceive
 
-  val props = Props(classOf[RequestReceiver], extractDevInfo, extractTargetInfo, policyValidator, enrichRequest,
+  val props = Props(classOf[ConnectionHandler], extractDevInfo, extractTargetInfo, policyValidator, enrichRequest,
     enrichResponse, errorHandler, forwardRequest)
 
   val httpServer = system.actorOf(props, "nozzle-server")
@@ -67,77 +64,22 @@ object DefaultHandlers {
   }
 }
 
-class RequestReceiver(
-                       devInfoExtractor: DevInfoExtractor,
-                       extractTargetInfo: TargetInfoExtractor,
-                       validatePolicy: ValidatePolicy,
-                       enrichRequest: RequestTransformer,
-                       enrichResponse: ResponseTransformer,
-                       errorHandler: ValidationFailureHandler,
-                       forwardRequest: ForwardRequest
-                       ) extends Actor with ActorLogging {
-
-  var origIp: Option[InetAddress] = None
-
+class ConnectionHandler(
+                         devInfoExtractor: DevInfoExtractor,
+                         extractTargetInfo: TargetInfoExtractor,
+                         validatePolicy: ValidatePolicy,
+                         enrichRequest: RequestTransformer,
+                         enrichResponse: ResponseTransformer,
+                         errorHandler: ValidationFailureHandler,
+                         forwardRequest: ForwardRequest
+                         ) extends Actor with ActorLogging {
   def receive = {
     // when a new connection comes in we register ourselves as the connection handler
     case c: Http.Connected =>
-      origIp = Some(c.remoteAddress.getAddress)
-      sender ! Http.Register(self)
-
-    case request: HttpRequest => {
-      val replyTo = sender
-
-      // Process getting developer and target info from the request in parallel
-      // and process them
-      val futureInfoExtractor = devInfoExtractor(request)
-      val futureTargetInfo = extractTargetInfo(request)
-
-      val futureResponse = for {
-        di <- futureInfoExtractor
-        ti <- futureTargetInfo
-        response <- handleInfos(request)((di, ti))
-      } yield response
-
-      futureResponse recover {
-        case t: Exception => errorHandler(t, request, None, None)
-      } onSuccess { case r => replyTo ! r }
-
-    }
-
-    case a => log.warning(a.toString)
-  }
-
-  private[this] def handleInfos(request: HttpRequest): ((DevInfo, Option[TargetInfo])) => Future[HttpResponse] = {
-
-    case (devInfo, Some(targetInfo)) => handleForwardRequest(request, devInfo, targetInfo)
-    case (_, None) => throw new NotFoundException(s"Rule not found to handle request for: ${request.uri}")
-
-  }
-
-  private[this] def handleForwardRequest(request: HttpRequest, devInfo: DevInfo, targetInfo: TargetInfo): Future[HttpResponse] = {
-    forwardRequest(addForwardedFromHeader(enrichRequest(request, devInfo, targetInfo))) map {
-      response => enrichResponse(request, response, devInfo, targetInfo)
-    } recover {
-      case e: Exception => errorHandler(e, request, Some(devInfo), Some(targetInfo))
-    }
-  }
-
-  protected[core] def addForwardedFromHeader(orig: HttpRequest) = {
-    origIp.map(_.getHostAddress).map{ ip =>
-      val xForwFor = "X-Forwarded-For"
-      val xForwForLower = xForwFor.toLowerCase
-
-      def xffFilter(h: HttpHeader) = h.lowercaseName == xForwForLower
-
-      val restHeaders = orig.headers.filterNot(xffFilter)
-
-      val xffHeaders = orig.headers.filter(xffFilter) match {
-          case Nil => List(RawHeader(xForwFor, ip))
-          case x :: Nil => List(RawHeader(x.name, s"${x.value}, ${ip}"))
-          case xs => xs :+ RawHeader(xForwFor, ip)
-      }
-      orig.copy(headers = restHeaders ::: xffHeaders)
-    }.getOrElse(orig)
+      val ipAddress = c.remoteAddress.getAddress
+      val requestHandler = context.actorOf(Props(classOf[RequestReceiver], devInfoExtractor,
+        extractTargetInfo, validatePolicy, enrichRequest,
+        enrichResponse, errorHandler, forwardRequest, ipAddress))
+      sender ! Http.Register(requestHandler)
   }
 }
---
+++ nozzle-core/src/main/scala/com/gilt/nozzle/core/RequestReceiver.scala
@@ -0,0 +1,80 @@
+package com.gilt.nozzle.core
+
+import com.gilt.nozzle.core.DevInfo._
+import com.gilt.nozzle.core.TargetInfo._
+import com.gilt.nozzle.core.PolicyValidator._
+import akka.actor.{ActorLogging, Actor}
+import java.net.InetAddress
+import spray.http.{HttpHeader, HttpResponse, HttpRequest}
+import scala.concurrent.Future
+import spray.http.HttpHeaders.RawHeader
+
+
+class RequestReceiver(
+                       devInfoExtractor: DevInfoExtractor,
+                       extractTargetInfo: TargetInfoExtractor,
+                       validatePolicy: ValidatePolicy,
+                       enrichRequest: RequestTransformer,
+                       enrichResponse: ResponseTransformer,
+                       errorHandler: ValidationFailureHandler,
+                       forwardRequest: ForwardRequest,
+                       ipAddress: InetAddress
+                       ) extends Actor with ActorLogging {
+
+  implicit lazy val ec = context.dispatcher
+
+  def receive = {
+
+    case request: HttpRequest =>
+      val replyTo = sender
+
+      // Process getting developer and target info from the request in parallel
+      // and process them
+      val futureInfoExtractor = devInfoExtractor(request)
+      val futureTargetInfo = extractTargetInfo(request)
+
+      val futureResponse = for {
+        di <- futureInfoExtractor
+        ti <- futureTargetInfo
+        response <- handleInfos(request)((di, ti))
+      } yield response
+
+      futureResponse recover {
+        case t: Exception => errorHandler(t, request, None, None)
+      } onSuccess { case r => replyTo ! r }
+
+    case a => log.warning(a.toString)
+  }
+
+  private[this] def handleInfos(request: HttpRequest): ((DevInfo, Option[TargetInfo])) => Future[HttpResponse] = {
+
+    case (devInfo, Some(targetInfo)) => handleForwardRequest(request, devInfo, targetInfo)
+    case (_, None) => throw new NotFoundException(s"Rule not found to handle request for: ${request.uri}")
+
+  }
+
+  private[this] def handleForwardRequest(request: HttpRequest, devInfo: DevInfo, targetInfo: TargetInfo): Future[HttpResponse] = {
+    forwardRequest(addForwardedFromHeader(enrichRequest(request, devInfo, targetInfo))) map {
+      response => enrichResponse(request, response, devInfo, targetInfo)
+    } recover {
+      case e: Exception => errorHandler(e, request, Some(devInfo), Some(targetInfo))
+    }
+  }
+
+  protected[core] def addForwardedFromHeader(orig: HttpRequest) = {
+    val ip = ipAddress.getHostAddress
+    val xForwFor = "X-Forwarded-For"
+    val xForwForLower = xForwFor.toLowerCase
+
+    def xffFilter(h: HttpHeader) = h.lowercaseName == xForwForLower
+
+    val restHeaders = orig.headers.filterNot(xffFilter)
+
+    val xffHeaders = orig.headers.filter(xffFilter) match {
+      case Nil => List(RawHeader(xForwFor, ip))
+      case x :: Nil => List(RawHeader(x.name, s"${x.value}, $ip"))
+      case xs => xs :+ RawHeader(xForwFor, ip)
+    }
+    orig.copy(headers = restHeaders ::: xffHeaders)
+  }
+}
