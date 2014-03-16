577070439c090029c55f3671aab0649dcf39d3b6
Javier Arrieta <jarrieta@gilt.com>
2014-03-16 16:53:38 +0000

    Corrected group-id for the artifacts
    Change-Id: Ied558357670a8828871982ffb7094b580ad05dc9

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -6,10 +6,10 @@ import java.util.Date
 object BuildSettings {
   val VERSION = "0.0.1"
 
-  lazy val basicSettings = seq(
+  lazy val basicSettings = Seq(
     version               := VERSION + new SimpleDateFormat("-yyyyMMdd").format(new Date),
     homepage              := Some(new URL("http://dev.gilt.com")),
-    organization          := "gilt.com",
+    organization          := "com.giltgroupe",
     organizationHomepage  := Some(new URL("http://www.gilt.com")),
     description           := "API gateway to publish your services to developers",
     startYear             := Some(2014),
